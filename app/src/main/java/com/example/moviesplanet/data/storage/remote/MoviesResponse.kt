56c111f621bdc350e1f5009f3d9900ab94f81ae6
viktork <viktor.kravchuk@playtech.com>
2020-06-16 22:17:04 +0300

    Null pointer crash fixed.

--- app/src/main/java/com/example/moviesplanet/data/storage/remote/MoviesResponse.kt
+++ app/src/main/java/com/example/moviesplanet/data/storage/remote/MoviesResponse.kt
@@ -7,7 +7,7 @@ data class MoviesResponse (
     @SerializedName("poster_path") val posterPath: String?,
     @SerializedName("adult") val adult: Boolean,
     @SerializedName("overview") val overview: String,
-    @SerializedName("release_date") val releaseDate: String,
+    @SerializedName("release_date") val releaseDate: String?,
     @SerializedName("genre_ids") val genreIds: List<Int>?,
     @SerializedName("id") val id: Int,
     @SerializedName("original_title") val originalTitle: String,
@@ -20,6 +20,6 @@ data class MoviesResponse (
     @SerializedName("vote_average") val voteAverage: Double) {
 
     fun toMovie() =
-        Movie(id, originalTitle, releaseDate, posterPath ?:"", voteAverage, overview)
+        Movie(id, originalTitle, releaseDate ?:"", posterPath ?:"", voteAverage, overview)
 
 }
