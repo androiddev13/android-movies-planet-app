2b53220ff164dbe3b95a4bab4dd5a698749fa644
viktork <viktor.kravchuk@playtech.com>
2020-07-14 21:47:35 +0300

    Unused classes removed.

--- app/src/main/java/com/example/moviesplanet/domain/GetSettingsUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.moviesplanet.domain
-
-import com.example.moviesplanet.data.MoviesRepository
-import javax.inject.Inject
-
-class GetSettingsUseCase @Inject constructor(private val moviesRepository: MoviesRepository) {
-}
